5a24a9c5d852e1e01f6ff570a089214ee5310b47
ined2011 <mayerlindained@gmail.com>
2025-09-13 14:53:31 -0500

    (develop) inicio de validación con expresiones regulares

---
+++ helpers/validarRegistro.js
@@ -0,0 +1,39 @@
+/*
+gerElementById
+gerElementById
+gerElementById
+gerElementById
+gerElementById
+gerElementById
+
+
+querySelector
+querySelector
+querySelector
+querySelector
+querySelector
+querySelector
+
+querySelectorAll: me permite seleccionar todo los elementos y con swit y poder elegir el que corresponde.
+
+for
+        getElement(id)
+
+*/
+
+/*let inputs = document.querySelectorAll("form inputs") /* Me recibe la sintaxos de la CSS*/
+
+let inputs = document.getElementsByClassName("formulario_entrada")
+
+let arregloInputs = [...inputs]
+console.log(inputs)
+console.log(arregloInputs)
+
+arregloInputs.map((input)=> {
+        console.log(input)
+}) /*funcion flecha
+
+
+/*inputs.map(fuction (){})*/
+
+/*console.log (inputs)*/
