3f64f1f8d6f65b79467008b8c77edc7d7a1bf5b6
ined2011 <mayerlindained@gmail.com>
2025-09-20 14:49:21 -0500

    (develope): Validación de formularios con expresiones regulares

---
+++ helpers/expresiones.js
@@ -0,0 +1,4 @@
+export let expresiones = {
+    texto:/^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]{5,50}$/
+    numero::/^\d{5,10}$/
+}
--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -1,37 +1,61 @@
-/*
-gerElementById
-gerElementById
-gerElementById
-gerElementById
-gerElementById
-gerElementById
+import{expresiones} from './expresiones.js' 
 
-
-querySelector
-querySelector
-querySelector
-querySelector
-querySelector
-querySelector
-
-querySelectorAll: me permite seleccionar todo los elementos y con swit y poder elegir el que corresponde.
-
-for
-        getElement(id)
-
-*/
-
-/*let inputs = document.querySelectorAll("form inputs") /* Me recibe la sintaxos de la CSS*/
-
-let inputs = document.getElementsByClassName("formulario_entrada")
+let inputs = document.getElementsByClassName("formulario__entrada")
 
 let arregloInputs = [...inputs]
-console.log(inputs)
-console.log(arregloInputs)
+/*console.log(inputs)
+console.log(arregloInputs)*/
 
 arregloInputs.map((input)=> {
-        console.log(input)
-}) /*funcion flecha
+        input.addEventListener("keyup",(e)=>{ 
+                switch(e.target.name){
+                case "nombre":
+                   if(expresiones.texto.test(e.target.value)){
+                        e.target.style.borderColor = "green"
+                        /*document.getElementById("nombre").style.color = "green"*/
+                        /*document.getElementById("nombre").style.background = "green"*/
+                    }else{
+                        document.getElementById("nombre").style.borderColor = "red"                  }
+                        document.getElementById("nombre").style.color = "red"
+                        break;
+                        
+                case "apellido":
+
+                    if(expresiones.texto.test(e.target.value)){
+                        e.target.classList.add("correcto") 
+                        e.target.classList.remove("incorrecto")       
+                        
+                        /*document.getElementById("apellido").style.color = "green"*/
+                    } else {
+                        e.target.classList.add("incorrecto")
+                        e.target.classList.remove("correcto")                        
+
+                        /*document.getElementById("apellido").style.color = "red"*/
+                    }
+                    break;  
+                
+                case "documento":
+                    if(expresiones.numero.test(e.target.value)){
+                       e.target.classList.add("correcto") 
+                       e.target.classList.remove("incorrecto") 
+                        /*document.getElementById("documento").style.color = "green"*/
+                    }else{
+                       e.target.classList.add("incorrecto")
+                       e.target.classList.remove("correcto")    
+                        /*document.getElementById("documento").style.color = "red"*/
+                    }
+                    break;    
+                }
+        
+                
+              })
+})
+
+
+
+
+
+        /*console.log(input)* /*funcion flecha
 
 
 /*inputs.map(fuction (){})*/
